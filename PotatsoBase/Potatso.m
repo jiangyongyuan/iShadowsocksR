628560597859ac120a08ff6f493d8ad7a52944d6
zlj <1534046381@qq.com>
2018-08-05 18:43:48 +0800

    切换账号证书

--- PotatsoBase/Potatso.m
+++ PotatsoBase/Potatso.m
@@ -14,7 +14,7 @@
     // reverted back
     // very curious why grabbing value from Info.plist will result in cannot connect to VPN all the time
     // it needs to be fixed like this as always
-    return @"group.com.ssrlive.zljssr";
+    return @"group.hk.wepn.ssr07";
 }
 
 + (NSURL *)sharedUrl {
